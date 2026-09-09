0d291b4f83de8898b750c4b2dadcdedd1ef2d84e
Tamas Andras Peter <tamas.andras@thestockmine.com>
2026-09-09 18:31:11 +0200

    ment

--- src/tests/inventoryClient.pact.test.ts
+++ src/tests/inventoryClient.pact.test.ts
@@ -12,9 +12,7 @@ const provider = new PactV3({
   dir: path.resolve(process.cwd(), 'pacts'),
 });
 
-
 describe('InventoryClient Pact Test', () => {
-
   it('fetches inventory item details by SKU', () => {
     provider
       .given('item execution exists for SKU-100')
@@ -30,14 +28,6 @@ describe('InventoryClient Pact Test', () => {
         body: InventoryMockFixtures.getItemResponseBody(),
       });
 
-    // 2. Attach generated Pact JSON file while the test context is active
-    const pactFilePath = path.resolve(process.cwd(), 'pacts', 'OrderService-InventoryService.json');
-    if (fs.existsSync(pactFilePath)) {
-      const pactContent = fs.readFileSync(pactFilePath, 'utf-8');
-      allure.attachment('Generated Pact Contract', pactContent, 'application/json');
-    }
-
-
     return provider.executeTest(async (mockServer) => {
       const client = new InventoryClient(mockServer.url);
       const data = await client.getStock('SKU-100');
@@ -45,8 +35,13 @@ describe('InventoryClient Pact Test', () => {
       expect(data.sku).toBe('SKU-100');
       expect(data.quantity).toBe(150);
       expect(data.status).toBe('IN_STOCK');
+
+      // NOW the test execution is complete and Pact has written the contract file!
+      const pactFilePath = path.resolve(process.cwd(), 'pacts', 'OrderService-InventoryService.json');
+      if (fs.existsSync(pactFilePath)) {
+        const pactContent = fs.readFileSync(pactFilePath, 'utf-8');
+        allure.attachment('Generated Pact Contract', pactContent, 'application/json');
+      }
     });
   });
-
- 
 });
